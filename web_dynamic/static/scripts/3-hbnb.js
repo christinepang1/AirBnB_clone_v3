0116981881ad42a6dc226644ab5e2171c5223ca1
christinepang1 <christinezpang@gmail.com>
2020-05-17 23:49:56 +0000

    Places JavaScript works now

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,6 +1,5 @@
 $(function () {
   const dict = {};
-
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
       dict[$(this).data('id')] = $(this).data('name');
@@ -10,39 +9,39 @@ $(function () {
     $('.amenities h4').text(Object.values(dict).join(', '));
   });
 });
-
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
-if (status === 'success') {
-  if (data.status === 'OK') {
-    $('#api_status').addClass('available');
+  if (status === 'success') {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+      $.ajax('http://0.0.0.0:5001/api/v1/places_search', {
+        type: 'POST',
+        contentType: 'application/json',
+        data: '{}',
+        success: data => {
+          for (const place of data) {
+            console.log(place);
+            $('section.places').append(
+              `<article>
+                <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">$${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                </div>
+                  </div>
+                  <div class="description">
+                  ${place.description}
+            </div>
+              </article>`
+            );
+          }
+        }
+      });
     } else {
       $('#api_status').removeClass('available');
-      }
-}
-
-$.ajax('http://0.0.0.0:5001/api/v1/places_search', {
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({}),
-    success: data => {
-      data.forEach(place => {
-	$('section.places').append(
-	  `<article>
-	      <div class="title_box">
-	        <h2>${place.name}</h2>
-	        <div class="price_by_night">$${place.price_by_night}</div>
-	      </div>
-	      <div class="information">
-	        <div class="max_guest">${place.max_guest} Guests</div>
-            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-	      </div>
-          </div>
-          <div class="description">
-	        ${place.description}
-	  </div>
-	    </article>`
-	  );
-	}
-	}
- );
+    }
+  }
+});
