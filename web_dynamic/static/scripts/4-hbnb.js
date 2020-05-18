c9c66c37dbda468effd338ef9049100808638a99
christinepang1 <christinezpang@gmail.com>
2020-05-18 00:25:41 +0000

    Implemented some of the button tag, braces though

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -7,7 +7,37 @@ $(function () {
       delete dict[$(this).data('id')];
     }
     $('.amenities h4').text(Object.values(dict).join(', '));
+
     $('button').click(() => {
+      $.ajax'(http://0.0.0.0:5001/api/v1/places_search', {
+	type: 'POST',
+	contentType: 'application/json',
+	data: JSON.stringify({amenities: Object.keys(dict)}),
+	success: data => {
+          for (const place of data) {
+            console.log(place);
+            $('section.places').append(
+              `<article>
+                <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">$${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                </div>
+                  </div>
+                  <div class="description">
+                  ${place.description}
+            </div>
+              </article>`
+	}
+}
+}});
+
+
+
     let obj = { amenities: [] }
     for (let key of Object.keys(list)) {
       obj.amenities.push(key)
